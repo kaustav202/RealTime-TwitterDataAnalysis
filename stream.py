bb64910e707450cb353998fb53ed6a979b1e561f
kaustav202 <89788120+kaustav202@users.noreply.github.com>
2021-11-13 12:09:00 +0530

    Create stream.py
    connects to the twitter API using tweepy  and writes stream to a file tweets.json

---
+++ stream.py
@@ -0,0 +1,94 @@
+
+import tweepy
+from tweepy import Stream
+
+# authorize the API Key
+authentication = tweepy.OAuthHandler(api_key, api_secret_key)
+
+# authorization to user's access token and access token secret
+authentication.set_access_token(access_token, access_token_secret)
+
+# call the api
+api = tweepy.API(authentication)
+
+#tweepy.models.Status
+
+
+
+
+from tweepy.streaming import StreamListener
+import json
+import time
+import sys
+
+class SListener(StreamListener):
+    def __init__(self, api = None, fprefix = 'streamer'):
+        self.api = api or API()
+        self.counter = 0
+        self.fprefix = fprefix
+        self.output  = open(self.fprefix , 'w')
+
+
+    def on_data(self, data):
+        if  'in_reply_to_status' in data:
+            self.on_status(data)
+        elif 'delete' in data:
+            delete = json.loads(data)['delete']['status']
+            if self.on_delete(delete['id'], delete['user_id']) is False:
+                return False
+        elif 'limit' in data:
+            if self.on_limit(json.loads(data)['limit']['track']) is False:
+                return False
+        elif 'warning' in data:
+            warning = json.loads(data)['warnings']
+            print("WARNING: %s" % warning['message'])
+            return
+
+
+    def on_status(self, status):
+        self.output.write(status)
+        self.counter += 1
+        if self.counter >= 20000:
+            self.output.close()
+            self.output  = open( self.fprefix , 'w')
+            self.counter = 0
+        return
+
+
+    def on_delete(self, status_id, user_id):
+        print("Delete notice")
+        return
+
+
+    def on_limit(self, track):
+        print("WARNING: Limitation notice received, tweets missed: %d" % track)
+        return
+
+
+    def on_error(self, status_code):
+        print('Encountered error with status code:', status_code)
+        return 
+
+
+    def on_timeout(self):
+        print("Timeout, sleeping for 60 seconds...")
+        time.sleep(60)
+        return 
+
+
+
+# Set up words to track
+keywords_to_track = ['#google', '#apple']
+
+# Instantiate the SListener object 
+
+file_to_write = "tweets.json"
+
+listen = SListener(api , file_to_write)
+
+# Instantiate the Stream object
+stream = Stream(authentication, listen)
+
+# Begin collecting data
+stream.filter(track = keywords_to_track)
+
