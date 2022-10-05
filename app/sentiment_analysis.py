981d5e0b847168b1502cb3850b78f4916a447b43
SegFal <92688849+segfal@users.noreply.github.com>
2022-10-05 09:26:43 -0400

    Update sentiment_analysis.py

--- app/sentiment_analysis.py
+++ app/sentiment_analysis.py
@@ -13,7 +13,7 @@ class SentimentalAnalysis:
         self.sentiment_scores = self.ds_tweets['text'].apply(self.sid.polarity_scores)
         self.sentiment = self.sentiment_scores.apply(lambda x: x["compound"])
 
-    def check_word_in_tweet(self,word, data):
+    def check_word_in_tweet(self,word, data):## This function checks columns for presence of keywords(boolean list)
        
         """Checks if a word is in a Twitter dataset's text. 
         Checks text and extended tweet (140+ character tweets) for tweets,
@@ -51,3 +51,7 @@ class SentimentalAnalysis:
         plt.title('Sentiment of Tweets');
         plt.legend(self.hashtag.keys())
         plt.show()
+
+
+
+sentiment = SentimentalAnalysis(ds_tweets=get_tweets())
