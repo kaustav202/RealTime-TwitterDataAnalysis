b5b65edf7fdcdfe88b27cdc15b1d62cd45c509cb
Manan Garg <manantechy@gmail.com>
2022-10-08 00:36:11 +0530

    log no of tweets after 50 writes instead of 1000

--- app/stream.py
+++ app/stream.py
@@ -49,7 +49,7 @@ class SListener(StreamListener):
     def on_status(self, status):
         self.output.write(status)
         self.counter += 1
-        if (self.counter % 1000 == 0):
+        if (self.counter % 50 == 0):
             print("No of tweets currently in tweets.json = ", self.counter)
         if self.counter >= 20000:
             self.output.close()
